dfbdeb5ba3e4182d57bed70e73136de8abf240bd
Alviska <insanelogic29@gmail.com>
2018-05-08 00:25:03 +0700

    Debug logout admin dan algoritma jiang-conrath

--- src/registration/urls.py
+++ src/registration/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView, PasswordChangeView,
 from allauth.account.views import LoginView, PasswordResetView, SignupView
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name="login"),
+    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='registration/logged_out.html'), name="logout"),
     # path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html', email_template_name='store/registration/password_reset_email.html'), name="password_reset"),
     # path('password_reset_confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name="password_reset_confirm"),
